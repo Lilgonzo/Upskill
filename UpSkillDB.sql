6c51c26fc9401deb3efa2f4d6662766860570f8f
aksil34 <ryan.liska@gmail.com>
2022-11-14 17:46:17 -0600

    Update Database

--- UpSkillDB.sql
+++ UpSkillDB.sql
@@ -2,7 +2,27 @@ create database IF NOT EXISTS UpSkillDB;
 use UpSkillDB;
 
 CREATE TABLE IF NOT EXISTS loginInfo(
-	user varchar (15) PRIMARY KEY, 
+	userID varchar(15) PRIMARY KEY,
+	user varchar (15), 
 	email varchar(15), 
 	pass varchar(15)
     );
+    
+CREATE TABLE IF NOT EXISTS profileInfo(
+	userID varchar(15) PRIMARY KEY,
+    bio varchar(15),
+    interests varchar(15),
+    dislikes varchar(15)
+    );
+    
+CREATE TABLE IF NOT EXISTS likes(
+	userID varchar(15) PRIMARY KEY,
+	likedUserID varchar(15)
+    );
+
+CREATE TABLE IF NOT EXISTS messages(
+	userID varchar(15) PRIMARY KEY,
+    userIDRecipient varchar(15),
+    message varchar(180)
+    );
+    
