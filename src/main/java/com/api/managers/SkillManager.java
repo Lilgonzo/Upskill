c02ad03d87ec4e7abb929bd915b362203e2a2568
Mario Gonzalez <56660056+Lilgonzo@users.noreply.github.com>
2022-12-03 21:47:52 -0600

    Update SkillManager.java

--- src/main/java/com/api/managers/SkillManager.java
+++ src/main/java/com/api/managers/SkillManager.java
@@ -1,1 +1,21 @@
+package com.api.managers;
+
+import com.api.entities.JWT;
+import com.api.entities.Profile;
+import com.api.utils.DbComm;
+import com.api.utils.JWTUtil;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class SkillManager(){
+
+}
+
 
